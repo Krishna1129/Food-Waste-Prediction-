d6d8267b280895d0a28433e136e651d8ae113910
Krishna1129 <your.email@example.com>
2026-02-17 18:17:01 +0530

    Remove unnecessary files - using Docker for deployment

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# Upgrade pip and setuptools
-pip install --upgrade pip setuptools wheel
-
-# Install Python dependencies
-pip install -r requirements.txt
