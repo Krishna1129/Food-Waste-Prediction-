4c3295e1645eb4a2e321e05210aea6e27099e820
Krishna1129 <your.email@example.com>
2026-02-17 17:45:54 +0530

    Fix: Specify Python 3.11.9 to resolve package compatibility issues

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip and setuptools
+pip install --upgrade pip setuptools wheel
+
 # Install Python dependencies
-pip install --upgrade pip
 pip install -r requirements.txt
